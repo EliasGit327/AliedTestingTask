2062c239f5224ffa932b02f5a630830f00006f42
Ilie <ilie.chukitu@gmail.com>
2020-10-13 11:04:11 +0300

    - Simple tests.

--- Test/PostRegistrationsTest.cs
+++ Test/PostRegistrationsTest.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using AlliedTestingTask;
+using AlliedTestingTask.Data.Models;
 using AlliedTestingTask.Data.Models.Requests;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Newtonsoft.Json;
@@ -26,12 +27,39 @@ namespace Test
         [Fact]
         public async Task PostRegistrationTest()
         {
+            var valve = new Organisation()
+            {
+                Name = "Valve",
+                Address = new Address()
+                {
+                    AddressLine1 = "Bellevue, Washington",
+                    CountryIsoCode = "3166-2",
+                    City = "San Francisco",
+                    State = "California",
+                    PostCode = "94016",
+                    Locale = "en"
+                }
+            };
+
+            var gabeNewel = new Person()
+            {
+                FirstName = "Gabe",
+                LastName = "Newel",
+                Email = "gaben@valvesoftware.com",
+                Address = new Address()
+                {
+                    Locale = "en",
+                    AddressLine1 = "San Francisco, M1257",
+                    CountryIsoCode = "A231321"
+                },
+            };
+            
             var regReq = new RegistrationRequest()
             {
                 RegistrationDate = DateTime.Now,
                 Locale = "en",
-                Organisation = null,
-                Person = null
+                Organisation = valve,
+                Person = gabeNewel
             };
             var parsedRegReq = await JsonConvert.SerializeObjectAsync(regReq);
 
@@ -43,7 +71,7 @@ namespace Test
             
             if (!response.IsSuccessStatusCode)
             {
-                _testOutputHelper.WriteLine(response.ToString());
+                _testOutputHelper.WriteLine(await response.Content.ReadAsStringAsync());
                 throw new InvalidOperationException("Bad request");
             }
             
